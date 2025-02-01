e7d26c246ff8e7c361cde9abe048212e213d9d4b
noah the goodra <peterpan0413@live.com>
2025-02-01 00:22:31 -0600

    add poweshell script for windows bros.

---
+++ BuildAllSpecs.ps1
@@ -0,0 +1,11 @@
+# Process all .spec files in the host's ./rpmbuild/SPECS directory
+Get-ChildItem -Path "./rpmbuild/SPECS" -Filter *.spec | ForEach-Object {
+    # Resolve the absolute host path for the volume mount
+    $hostPath = (Resolve-Path "./rpmbuild").Path
+    
+    # Run Docker with the resolved path and container file reference
+    docker run -it --rm `
+        -v "${hostPath}:/root/rpmbuild" `
+        cookme `
+        "/root/rpmbuild/SPECS/$($_.Name)"
+}
